70d18d45b740a6e61540ff9e772043b54113716e
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-30 21:52:38 +0300

    Add friendlist component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile';
 import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 export const App = () => {
   return (
@@ -24,6 +26,8 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
     </div>
   );
 };
