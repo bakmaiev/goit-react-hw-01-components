a462f871c34806bebd254cdad5277d2e2c58d6ef
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-30 17:53:51 +0300

    Add new changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import { Profile } from './Profile';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import { Statistics } from './Statistics';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +12,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
