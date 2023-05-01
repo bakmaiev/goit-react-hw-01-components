653ff34f48191e1c94fe260d126328f8576126b8
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-01 18:40:22 +0300

    Add new styles to transaction components

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.title}>Type</th>
+          <th className={css.title}>Amount</th>
+          <th className={css.title}>Currency</th>
         </tr>
       </thead>
       <tbody>
         {items.map(item => {
           return (
-            <tr key={item.id}>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
+            <tr key={item.id} className={css.list}>
+              <td className={css.item}>{item.type}</td>
+              <td className={css.item}>{item.amount}</td>
+              <td className={css.item}>{item.currency}</td>
             </tr>
           );
         })}
